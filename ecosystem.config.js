a75693e786ea9786cc24bcce78b33d4340bc9232
Gusto <lhg0616@gmail.com>
2025-09-28 18:10:29 +0900

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "btc-bot",
       cwd: "/home/ubuntu/upbit-bot", // <- 프로젝트 절대경로로 맞추기
       script: "dist/bot.js", // <- 빌드 산출물 실행 (권장)
-      env_file: ".env", // <- 여기서 .env를 로드
+      env_file: "/home/ubuntu/upbit-bot/.env", // <- 여기서 .env를 로드
       node_args: ["--enable-source-maps"], // 선택
       max_restarts: 20,
       restart_delay: 3000,
